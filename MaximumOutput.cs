e229c9caf7cc2ac61a15b8b2174a1c5c243765be
Abdul-Lateef Adeleye <abdul-lateef.adeleye@prunedge.com>
2022-05-10 10:28:00 +0100

    Add project files.

---
+++ MaximumOutput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanCodingTest
+{
+    public class MaximumOutput
+    {
+       
+
+        public int findMaxValuesStolen(int[] arr)
+        { 
+            int output;
+            int n = arr.Length;
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return arr[0];
+            if (n!=2)
+            {
+
+                int val1 = arr[0];
+                int val2 = Math.Max(arr[0], arr[1]);
+                int i = 2;
+
+                do
+                {
+                    output = Math.Max((arr[i] + val1), val2);
+                    val1 = val2;
+                    val2 = output;
+                    i++;
+
+                } while (i < n);
+
+                return output;
+            }
+            else
+            {
+                return Math.Max(arr[0], arr[1]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using DanCodingTest;
+using System.Diagnostics;
+
+
+MaximumOutput sum = new MaximumOutput();
+Random random = new Random();
+int[] arr = { 2,1 };
+//var arr = (from p in Enumerable.Range(0, 5000000)
+//           select random.Next(0, 99)).ToArray();
+var watch = Stopwatch.StartNew();
+int n = arr.Length;
+var output =sum.findMaxValuesStolen(arr);
+Console.WriteLine(output);
+watch.Stop();
+Console.WriteLine($"Execution Time:{watch.ElapsedMilliseconds} ms");
+
+
+
